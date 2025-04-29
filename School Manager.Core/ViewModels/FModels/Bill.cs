9120b01bd5468c994a76cf330d9dd7eeb0707db2
mehrab ahmadi <mehrabzi431@gmail.com>
2025-04-29 23:29:16 +0330

    Adding Bill And Fixing Some Classes

---
+++ School Manager.Core/ViewModels/FModels/Bill.cs
@@ -0,0 +1,12 @@
+﻿namespace School_Manger.Models
+{
+    public class Bill
+    {
+        public int Id { get; set; }
+        public int ChildId { get; set; }
+        public long TotalPrice { get; set; }
+        public long PaidPrice { get; set; }
+        public bool HasPaId {  get; set; }
+        public DateTime PaidTime { get; set; }
+    }
+}
--- School Manger/Models/Driver.cs
+++ School Manger/Models/Driver.cs
@@ -1,10 +1,25 @@
 ﻿namespace School_Manger.Models
 {
+    public enum CarType
+    {
+        Personal,
+        Taxi,
+        Van,
+        Bus
+    }
     public class Driver
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string LastName { get; set; }
+        public string FutherName { get; set; }
+        public DateTime BirthDate {  get; set; }
+        public string CertificateId { get; set; }
+        public string Education { get; set; }
+        public string Descriptions { get; set; }
+        public string Address { get; set; }
+        public string BankAccount { get; set; }
+        public string BankNumber { get; set; }
         public string NationCode { get; set; }
         public List<ChildInfo> Passanger { get; set; }
         public CarInfo Car { get; set; }
@@ -14,6 +29,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string PlateNumber { get; set; }
+        public string Color { get;set; }
         public int AvailableSeats { get; set; }
         public int SeatNumber { get; set; }
     }
