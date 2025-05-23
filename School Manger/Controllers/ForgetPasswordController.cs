9ef91749b37226687239853cd1276339b7f291e5
mehrab ahmadi <mehrabzi431@gmail.com>
2025-05-24 01:57:41 +0330

    Adding Forget Passwrod & End Of User

---
+++ School Manger/Controllers/ForgetPasswordController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace School_Manger.Controllers
+{
+    public class ForgetPasswordController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult OTPConfirmation()
+        {
+            return View();
+        }
+        public IActionResult ResetPassword()
+        {
+            return View();
+        }
+    }
+}
