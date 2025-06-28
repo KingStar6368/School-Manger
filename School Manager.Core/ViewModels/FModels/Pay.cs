c28c25ae28a00605b81210c3ab6ad7fa3ce43c81
bonyanco <abolfazlgamyousefi@gmail.com>
2025-06-28 16:15:11 +0330

    tracking code

--- School Manager.Core/ViewModels/FModels/Pay.cs
+++ School Manager.Core/ViewModels/FModels/Pay.cs
@@ -29,14 +29,14 @@ namespace School_Manager.Core.ViewModels.FModels
         public DateTime BecomingTime { get; set; }
         public PayType PayType { get; set; }
         public List<long> Bills { get; set; }
-        public string TrackingCode { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string TrackingCode { get; set; }
     }
     public class PayUpdateDto : IPayDto
     {
         public long Price { get; set; }
         public DateTime BecomingTime { get; set; }
         public PayType PayType { get; set; }
-        public string TrackingCode { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string TrackingCode { get;set; }
     }
     public class PayBillDto
     {
