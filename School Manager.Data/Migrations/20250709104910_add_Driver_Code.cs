aa141b56334eb78aad1fcf412730efe995e3bc6a
bonyanco <abolfazlgamyousefi@gmail.com>
2025-07-09 14:20:06 +0330

    add migration

---
+++ School Manager.Data/Migrations/20250709104910_add_Driver_Code.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace School_Manager.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_Driver_Code : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Code",
+                table: "Drivers",
+                type: "nvarchar(50)",
+                nullable: true,
+                comment: "کد پرونده");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Code",
+                table: "Drivers");
+        }
+    }
+}
