f538463c0b3c475d2143e32ec329e1b889e441fc
King Star <mggg6368@gmail.com>
2025-07-21 18:48:09 +0330

    Fix Bug

--- School Manager.Core/ViewModels/FModels/User.cs
+++ School Manager.Core/ViewModels/FModels/User.cs
@@ -24,7 +24,10 @@ namespace School_Manager.Core.ViewModels.FModels
         public string FirstName { get; set; }
         public string Mobile { get; set; }
         public string LastName { get; set; }
-        public string FullName { get; set; }
+        public string FullName
+        {
+            get => FirstName + " " + LastName;
+        }
         public bool IsActive { get; set; }
         public string Status => IsActive ? "فعال" : "غیرفعال";
 
