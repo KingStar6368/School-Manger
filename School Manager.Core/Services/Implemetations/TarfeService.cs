8039accd852e2545fe0bde75504e48f58decbff5
King Star <mggg6368@gmail.com>
2025-07-28 12:59:09 +0330

    Adding Tarfe Service

---
+++ School Manager.Core/Services/Implemetations/TarfeService.cs
@@ -0,0 +1,23 @@
+﻿using School_Manager.Core.Services.Interfaces;
+using School_Manager.Core.ViewModels.FModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School_Manager.Core.Services.Implemetations
+{
+    public class TarfeService : ITarfePrice
+    {
+        public TarfePriceDto GetTarfe(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TarfePriceDto GetTarfeByKm(float Km)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ School Manager.Core/Services/Interfaces/ITarfePrice.cs
@@ -0,0 +1,25 @@
+﻿using School_Manager.Core.ViewModels.FModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School_Manager.Core.Services.Interfaces
+{
+    public interface ITarfePrice
+    {
+        /// <summary>
+        /// گرفتن تعرفه با کد
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        public TarfePriceDto GetTarfe(int Id);
+        /// <summary>
+        /// گرفتن تعرفه با نزدیک ترین کیلومتر
+        /// </summary>
+        /// <param name="Km">کیلومتر ورودی</param>
+        /// <returns></returns>
+        public TarfePriceDto GetTarfeByKm(float Km);
+    }
+}
