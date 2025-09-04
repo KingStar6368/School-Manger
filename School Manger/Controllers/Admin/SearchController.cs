8e3cf1bef054c070189eb09fa14f478172b9b548
mehrab ahmadi <mehrabzi431@gmail.com>
2025-09-04 15:31:47 +0330

    Adding Serach Page

---
+++ School Manger/Controllers/Admin/SearchController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using School_Manager.Core.Services.Interfaces;
+using School_Manager.Core.ViewModels.FModels;
+using System.Threading.Tasks;
+
+namespace School_Manger.Controllers.Admin
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class SearchController : Controller
+    {
+        private IParentService parentService;
+        private IChildService childService;
+        private IDriverService driverService;
+        public SearchController(IChildService childService, IDriverService driverService,IParentService parentService)
+        {
+            this.childService = childService;
+            this.driverService = driverService;
+            this.parentService = parentService;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Driver(SearchDto searchDto)
+        {
+            return View(await driverService.SearchDriver(searchDto));
+        }
+        [HttpPost]
+        public async Task<IActionResult> Parent(SearchDto searchDto)
+        {
+            return View(await parentService.SearchParents(searchDto));
+        }
+        [HttpPost]
+        public async Task<IActionResult> Child(SearchDto searchDto)
+        {
+            return View(await childService.SearchChild(searchDto));
+        }
+    }
+}
