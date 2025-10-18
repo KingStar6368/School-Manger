41447e353f03573678b636f8569492d4786114e8
mehrab ahmadi <mehrabzi431@gmail.com>
2025-10-19 01:04:04 +0330

    Adidng Migrations

--- School Manager.Data/Migrations/20251018213345_Change Bills Bills Beheviore Resited_Cascade.Designer.cs
+++ School Manager.Data/Migrations/20251018213345_Change Bills Bills Beheviore Resited_Cascade.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using School_Manager.Data.Context;
 
@@ -11,9 +12,11 @@ using School_Manager.Data.Context;
 namespace School_Manager.Data.Migrations
 {
     [DbContext(typeof(SchMSDBContext))]
-    partial class SchMSDBContextModelSnapshot : ModelSnapshot
+    [Migration("20251018213345_Change Bills Bills Beheviore Resited_Cascade")]
+    partial class ChangeBillsBillsBehevioreResited_Cascade
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ School Manager.Data/Migrations/20251018213345_Change Bills Bills Beheviore Resited_Cascade.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace School_Manager.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeBillsBillsBehevioreResited_Cascade : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bills_ServiceContracts_ServiceContractRef",
+                table: "Bills");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bills_ServiceContracts_ServiceContractRef",
+                table: "Bills",
+                column: "ServiceContractRef",
+                principalTable: "ServiceContracts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bills_ServiceContracts_ServiceContractRef",
+                table: "Bills");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bills_ServiceContracts_ServiceContractRef",
+                table: "Bills",
+                column: "ServiceContractRef",
+                principalTable: "ServiceContracts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
